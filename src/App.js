6c11d7990d9e34cbed24dd1c3d9c987e70ba5e20
Alex <necrasoff.alex@yandex.ru>
2022-07-10 23:29:54 +0500

    add react props card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import Card from "./components/Card";
 import ShadowBox from "./components/ShadowBox";
 import Header from "./components/Header";
 
+const arr = [
+  {name: "Мужские кроссовки Nike Blazer Mid Suede", imageUrl: "/img/sneakers/1.jpg" , price: 12990},
+  {name: "Мужские Кроссовки Nike Air Max 270",imageUrl: "/img/sneakers/2.jpg" , price: 10990},
+  {name: "Мужские Кроссовки Nike Blazer Mid Suede",imageUrl: "/img/sneakers/3.jpg" , price: 8990},
+  {name: "Кроссовки Puma X Aka Boku Future Rider",imageUrl: "/img/sneakers/4.jpg" , price: 12990}
+]
+
 
 
 function App() {
@@ -9,23 +16,23 @@ function App() {
     <div className="wrapper clear">
 <ShadowBox />
 <Header />
-     <div className="content p-40">
-      <div className="d-flex justify-between">
-        <h1> Super SNEAKERS for YOU </h1>
 
-        <div className="search-block ">
-          <img width={14} height={14} src="/img/search.svg" alt="Search" />
-          <input placeholder="Поиск ..." />
+      <div className="content p-40">
+        <div className="d-flex justify-between">
+          <h1> Super SNEAKERS for YOU </h1>
+
+          <div className="search-block ">
+            <img width={14} height={14} src="/img/search.svg" alt="Search" />
+            <input placeholder="Поиск ..." />
+          </div>
         </div>
-      </div>
 
-       <div className="d-flex justify-between flex-wrap ">
+          <div className="d-flex justify-between flex-wrap ">
 
-<Card />
-
-       </div>  
-      </div> 
-
+            {arr.map(obj => <Card title = {obj.name} imageUrl = {obj.imageUrl} price = {obj.price} />)}
+            
+            </div>  
+       </div> 
     </div>
   );
 }
--- src/components/ShadowBox.js
+++ src/components/ShadowBox.js
@@ -1,6 +1,6 @@
 function ShadowBox () {
     return (
-        <div  className="shadow-box">
+        <div style={{display: 'none'}} className="shadow-box">
         <div className="right-box">
           <div className="header-basket d-flex justify-between align-center">
             <h2>Корзина</h2>
@@ -10,17 +10,17 @@ function ShadowBox () {
 </svg></button>
             </div>
               <div className="items">
-              <div className="cart-item d-flex align-center justify-around mb-20">
-              <img width={70} height={70} src="/img/sneakers/1.jpg" alt="1" />
-              <div>
-                <p>Мужские кроссовки <br/> Nike Blazer Mid Suede</p>
-                <b>12 999 руб.</b>
-              </div>
-            <button><svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect x="0.5" y="0.5" width="31" height="31" rx="7.5" fill="white" stroke="#DBDBDB"/>
-<path d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z" fill="#B5B5B5"/>
-</svg></button>
-            </div>
+                <div className="cart-item d-flex align-center justify-around mb-20">
+                <img width={70} height={70} src="/img/sneakers/1.jpg" alt="1" />
+                <div>
+                    <p>Мужские кроссовки <br/> Nike Blazer Mid Suede</p>
+                    <b>12 999 руб.</b>
+                </div>
+                <button><svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="0.5" y="0.5" width="31" height="31" rx="7.5" fill="white" stroke="#DBDBDB"/>
+    <path d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z" fill="#B5B5B5"/>
+    </svg></button>
+                </div>
             <div className="cart-item d-flex align-center justify-around mb-20">
               <img width={70} height={70} src="/img/sneakers/1.jpg" alt="1" />
               <div>
